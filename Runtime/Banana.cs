b5357ce4991344d57cfdeb5e5dd1b2c8f7335048
keonkuk.park <keonkuk.park@formalworks.com>
2024-09-30 11:45:56 +0900

    [update] 미끄러지는 기능 구현

--- Runtime/Banana.cs
+++ Runtime/Banana.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Banana : MonoBehaviour
 {
+    private Rigidbody m_rigidbody;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Banana!!");
+        if (m_rigidbody == null)
+        {
+            return;
+        }
+
+        StartCoroutine(StartAnimation());
+    }
+
+    private IEnumerator StartAnimation()
+    {
+        var dir = Random.onUnitSphere;
+        dir.y = 0;
+        dir = dir.normalized * 10f;
+
+        m_rigidbody.AddForce(dir, ForceMode.Impulse);
+
+        yield break;
     }
 }
